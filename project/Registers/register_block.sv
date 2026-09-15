8754fdacdfaa613990ec609e707519406d6617d7
Vidar Nahlbom <vidar.nahlbom@gmail.com>
2026-09-15 12:23:32 +0200

    Renamed register to register_file, commented and debugged and simplifed and renamed variables in register_file. Renamed variables in register_block

--- project/Registers/register_block.sv
+++ project/Registers/register_block.sv
@@ -3,28 +3,23 @@
 module register_block #(parameter BW) (
     // ----------INPUTS-------------
     input logic clk,                // clock signal
-    input logic reset,              // Resets the registers output to all 0, Active LOW
-    input logic write,              // Witrte signal to read state, commonly also called enable, Active LOW
+    input logic rst_n,              // Resets the registers output to all 0, Active LOW
+    input logic write_en_n,         // Write signal to read state, commonly also called enable, Active LOW
     input logic [BW-1:0] data_in,   // Input data to be read on the rising edge of the clock if write enabled
     // ----------OUTPUTS------------
     output logic [BW-1:0] out
 );
 
-// Since this is a D flip flop the ciruit only reacts to a changing clock depending on what write has for value.
-// This means that a always_ff block is used
+    // Since this is a D flip flop the ciruit only reacts to a changing clock depending on what write has for value.
+    // This means that a always_ff block is used
 
-always_ff @(posedge clk or negedge reset) begin
-    
-    // Handle reset, it takes precidence over writes
-    if(!reset) begin
-
-        out <= 'b0;
-    // Handle a write event
-    end else if (!write) begin
-        
-        out <= data_in;
-
+    always_ff @(posedge clk or negedge rst_n) begin
+        // Handle reset, it takes precidence over writes
+        if(!rst_n) begin
+            out <= 'b0;
+        // Handle a write event
+        end else if (!write_en_n) begin
+            out <= data_in;
+        end
     end
-
-end
 endmodule
---
+++ project/Registers/register_file.sv
@@ -0,0 +1,80 @@
+
+// Import the register block module
+
+// Module meant to simualte the registers of a RISC V processor. Course nomenclature said this is to be called register_file.
+// Not to be confused with register_block witch is a singular register.
+module register_file #(parameter BW, parameter DEPTH) (
+    //--------------------INPUTS--------------------
+    input logic clk,                            // Clock input
+    input logic rst_n,                          // Asynchronous active low reset signal
+    input logic write_en_n,                     // Active low write enable
+    input logic chip_en,                        // Active high chip enable
+    input logic [BW-1:0] data_in,               // Data input port
+    input logic [$clog2(DEPTH)-1] read_addr_1,    // Address for output port 1
+    input logic [$clog2(DEPTH)-1] read_addr_2,    // Address for output port 2
+    input logic [$clog2(DEPTH)-1] write_addr,     // Address for input port
+    //--------------------OUTPUTS--------------------
+    output logic [BW-1:0] data_out_1,           // Data output for port 1
+    output logic [BW-1:0] data_out_2            // data output for port 2
+);
+    // Using generate I can tell the simulator to make a desiered number of copies from a given circuit/module
+    // Make an array from all the registers with write_enables and outputs. This way data is easily routed.
+
+    logic write_enables [DEPTH];
+    logic [BW-1:0] outputs [DEPTH];
+    // So these are effectively wires connected to the respective ports on the register_blocks.
+
+    // Ensuring that reading from 0 always returns a 0 and not X or Z
+    assign outputs[0] = 'b0;
+
+    // Generate the desired number of register instances.
+    // Starting from i = 1 since address 0 is hardwired to 0.
+    // Remember address = 0 is handled separately.
+    generate
+        for (genvar i = 1; i < DEPTH; i++ ) begin
+
+            // An instance of a singular register
+            register_block #(.BW(BW)) internal_reg (
+                .clk(clk),
+                .rst_n(rst_n),
+                .write_en_n(write_enables[i]),
+                .data_in(data_in),
+                .out(outputs[i])
+            );
+            
+        end
+    endgenerate
+
+    // Since the routing of data does not depend on prior inputs the always_comb block is used.
+    always_comb begin
+
+        // As a default, write to all blocks is disabled
+        // Since its active low, this means value is set to 1
+        write_enables = '{default: 1'b1};
+
+        // Reset function & Standby function
+        // For the RF, both functions cause the same effect.
+        // For the reset function, since rst has already been wired to each internal_reg
+        // rst is handled internally by each. 
+        // but we still need to reset the data_out
+        // this is done combinationally and therefore async here
+        // in the reg blocks, its activated by rst_n having negedge
+        if (!rst_n | !chip_en) begin
+            data_out_1 = 'b0;
+            data_out_2 = 'b0;
+        end
+
+        // Write function & Read function
+        // We handle write and read at the same time, as no matter what data is read to the outputs.
+        else begin
+            if (!write_en_n) begin 
+                // overwrite 0 to enable write function of that specific internal reg.
+                // Because write signals are active low its set to 0
+                write_enables[write_addr] = '0;
+            end
+            // the outputs are determined by the corresponding address provided.
+            data_out_1 = outputs[read_addr_1];
+            data_out_2 = outputs[read_addr_2];
+        end
+    end
+endmodule
